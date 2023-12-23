7785d859fbefb8756bb6ca7217eaef0462c6390b
Makyno,Yskz <yasukazu@makyno.jp>
2023-12-24 07:51:22 +0900

    open local terminal's localhost in VS Code terminal

--- bottle-test.py
+++ bottle-test.py
@@ -1,8 +1,9 @@
+# Open local terminal's localhost:8080 if running in VS Code terminal of remote host
 from bottle import route, run
 
 @route('/')
 def myhome():
 
-    return "It's my home page"
+    return "<h1>It's my home page</h1>"
 
-run(host='localhost', port=8008)
+run(host='localhost', port=8080)
