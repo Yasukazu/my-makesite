99b84467f78baa1f726b5f2d863bf98e6afd7af1
Makyno,Yskz <yasukazu@makyno.jp>
2023-12-24 07:53:29 +0900

    output of running : get / http/1.1 200 26

--- bottle-test.py
+++ bottle-test.py
@@ -1,4 +1,10 @@
 # Open local terminal's localhost:8080 if running in VS Code terminal of remote host
+"""python bottle-test.py 
+Bottle v0.12.25 server starting up (using WSGIRefServer())...
+Listening on http://localhost:8080/
+Hit Ctrl-C to quit.
+
+127.0.0.1 - - [24/Dec/2023 07:47:45] "GET / HTTP/1.1" 200 26"""
 from bottle import route, run
 
 @route('/')
