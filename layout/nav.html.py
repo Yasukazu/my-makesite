7364b28c0b30e353db2771e2c8a14997e10d72cf
Makyno,Yskz <yasukazu@makyno.jp>
2023-12-26 06:50:42 +0900

    content_obj

--- layout/nav.html.py
+++ layout/nav.html.py
@@ -20,8 +20,7 @@ def render_a():
         yield hg.A(v + ' ' + k, href="{{ base_path }}/%s/"%k)
         #list.append(hg.A(v + ' ' + k, href="{{ base_path }}/%s/"%k))
     #return list
-
-nav_obj = hg.NAV(
+content_obj = hg.NAV(
     hg.A(
         index_title,
         href="{{ base_path }}/",
@@ -29,5 +28,7 @@ nav_obj = hg.NAV(
     *[a for a in render_a()] #(a for a in render_a({}))
     )
 if __name__ == '__main__':
-  nav_txt = render(nav_obj,{})
-  print(nav_txt)
+  #nav_txt = render(nav_obj,{})
+  body_obj = hg.BODY(content_obj, hg.MAIN("In main."))
+  body_txt = render(body_obj,{})
+  print(body_txt)
