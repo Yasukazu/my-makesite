8dd4538133f3d513e37edcc0e0680f2f90e0f955
Makyno,Yskz <yasukazu@makyno.jp>
2023-12-25 16:41:46 +0900

    datetime.date, tag of main

--- content/how_to_write.py
+++ content/how_to_write.py
@@ -1,26 +1,26 @@
-import pyhtml as h
+#import htbuilder as h
+from htbuilder import main, h1, h2, ul, li, pre
 #from pyhtml import *
-meta = { 'title' : "How to write",
-     'subtitle' : "Contents format" }
-html = h.html(
-      h.body(
-        h.h2("Difference from original makesite"),
-        h.ul(
-            h.li("Originally, meta data <front matters> are set as comments of HTML."),
-            h.li("In this fork, meta data in **markdown** files may be written in leading 3-hyphen separated part as YAML format(YAML format is like an intend format version JSON format). Like:")
+import datetime
+date = datetime.date.fromisoformat("2023-12-25")
+tag = main(
+    h1("Formats of documents"),
+        h2("Difference from original makesite"),
+        ul(
+            li("Originally, meta data <front matters> are set as comments of HTML."),
+            li("In this fork, meta data in **markdown** files may be written in leading 3-hyphen separated part as YAML format(YAML format is like an intend format version JSON format). Like:")
         ),
-        h.pre(
+        pre(
         """
         ---
         title: a title of an article
         ---
         """),
-        h.h2("Headings(H2, H3, ... H6)"),
-        h.ul(
-            h.li("Headings start from", h.b("H2"), "tag since", h.b("H1"), "is set with _title_ metadeta in the front matter.")
+        h2("Headings(H2, H3, ... H6)"),
+        ul(
+            li("Headings start from", b("H2"), "tag since", b("H1"), "is set with _title_ metadeta in the front matter.")
         ),
-        h.h2("Template")
+        h2("Template")
       )
-)
-output = html.render()
-print(output)
+#output = html.render()
+#print(output)
