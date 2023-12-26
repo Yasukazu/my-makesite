8bfb75cabf66211738623782c0282e154b94f1d9
Makyno,Yskz <yasukazu@makyno.jp>
2023-12-27 00:10:47 +0900

    insert(0,cwd);import layout.nav_html as nav_mod

--- layout/body_html.py
+++ layout/body_html.py
@@ -3,9 +3,10 @@ from htmlgenerator import mark_safe
 import os
 cwd = os.getcwd()
 import sys
-sys.path.append(cwd)
-from module_util import imp_mod_fm_file_loc
-nav_mod = imp_mod_fm_file_loc('nav_html', 'layout/nav_html.py')
+sys.path.insert(0, cwd) # append
+# from module_util import imp_mod_fm_file_loc
+import layout.nav_html as nav_mod
+# nav_mod = imp_mod_fm_file_loc('nav_html', 'layout/nav_html.py')
 content_obj = hg.BODY(
         nav_mod.content_obj,
         hg.MAIN(
