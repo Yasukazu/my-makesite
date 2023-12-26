6325d7451b3a520b5e024db344dfbfcb41536eff
Makyno,Yskz <yasukazu@makyno.jp>
2023-12-26 17:57:10 +0900

    FOOTER;mark_safe

--- layout/body_html.py
+++ layout/body_html.py
@@ -1,6 +1,5 @@
 import htmlgenerator as hg
-from htmlgenerator import mark_safe as safe
-from htmlgenerator import render
+from htmlgenerator import mark_safe
 import os
 cwd = os.getcwd()
 import sys
@@ -8,9 +7,29 @@ sys.path.append(cwd)
 from module_util import imp_mod_fm_file_loc
 nav_mod = imp_mod_fm_file_loc('nav_html', 'layout/nav_html.py')
 content_obj = hg.BODY(
-    nav_mod.content_obj,
-    "{{ content }}"
-    )
+        nav_mod.content_obj,
+        hg.MAIN(
+            "\n{{ content }}\n",
+        ),
+        hg.FOOTER(
+            hg.P(
+              mark_safe("&copy;"),
+              "{{ current_year }} Yskz", #"\xa9
+            ),
+            hg.P(
+                hg.A(
+                    hg.B(
+                        "GitHub",
+                    ),
+                    " repository page",
+                    href="https://github.com/Yasukazu/my-makesite",
+                ),
+            ),
+        ),
+        id="{{ slug }}",
+)
+
 if __name__ == '__main__':
+  from htmlgenerator import render
   content_txt = render(content_obj,{})
   print(content_txt)
